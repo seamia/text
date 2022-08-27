2bdc09bb077bcb2b50b7938e37717ea582bc86ed
nontechno <github@nontechno.com>
2022-08-27 15:18:51 -0700

    remove

--- inBetween.go
+++ inBetween.go
@@ -8,14 +8,35 @@ import (
 	"strings"
 )
 
+/**
+	Finds first occurence of text between specified markers, e.g.
+	whole:		"prefix [ center ] suffix""
+	start:		"["
+	end:		"]"
+	returns:	" center ", true
+ */
 func FindInBetween(whole, start, end string) (string, bool) {
 
-	if a := strings.Index(whole, start); a >= 0 {
-		remains := whole[a+len(start):]
-		if b := strings.Index(remains, end); a >= 0 {
-			return remains[:b], true
+	if left := strings.Index(whole, start); left >= 0 {
+		remains := whole[left+len(start):]
+		if right := strings.Index(remains, end); right >= 0 {
+			return remains[:right], true
 		}
 	}
 	return "", false
 }
 
+func RemoveInBetween(whole, start, end string, includingMarkers bool) string {
+
+	if left := strings.Index(whole, start); left >= 0 {
+		remains := whole[left+len(start):]
+		if right := strings.Index(remains, end); right >= 0 {
+			if includingMarkers {
+				return whole[:left] + whole[left+len(start)+right+len(end):]
+			} else {
+				return whole[:left+len(start)] + whole[left+len(start)+right:]
+			}
+		}
+	}
+	return whole
+}
